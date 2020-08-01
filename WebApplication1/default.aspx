de8683826e47a9958e20c999abf7df73e425ad9e
Yugaank Sharma <yugaanks@hotmail.com>
2020-08-01 13:53:17 -0500

    appform-template

---
+++ WebApplication1/default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="AppFormGenerator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label Text="Select Form" runat="server" />
+        <asp:DropDownList runat="server" ID="forms" OnSelectedIndexChanged="Form_Selection_Change" AutoPostBack="true">
+            <asp:ListItem Selected="True" Text="" />
+            <asp:ListItem Text="EIANotification" Value="EIANotification" />
+        </asp:DropDownList>
+        <asp:Panel runat="server" ID="formPanel" Visible="false">
+            <div>
+                <asp:Label runat="server" ID="FirstName">First Name:</asp:Label>
+                <asp:TextBox runat="server" ID="CandidateFirstName"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Label Text="Last Name" ID="LastName" runat="server" />
+                <asp:TextBox runat="server" ID="CandidateLastName" />
+            </div>
+            <div>
+                <asp:Button Text="Submit" ID="Submit" runat="server" OnClick="Submit_button_Click" />
+            </div>
+            <div>
+                <asp:Button Text="Draft Email" ID="SendEmail" runat="server" OnClick="SendEmail_button_Click" Enabled="false" />
+            </div>
+            <div>
+                <asp:Label ID="Template" runat="server" />
+            </div>
+        </asp:Panel>
+    </form>
+
+</body>
+</html>
---
+++ WebApplication1/default.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using System.Windows.Forms;
+
+namespace AppFormGenerator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                clearFields();
+            }
+        }
+        
+        protected void Submit_button_Click(object sender, EventArgs e)
+        {
+            
+            if(firstName == string.Empty || lastName == string.Empty)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "ErrorAlert", "alert('Please enter your First and Last name')", true);
+                clearFields();
+                return;
+            }
+            generateText();
+        }
+
+        protected void SendEmail_button_Click(object sender, EventArgs e)
+        {
+            var to = "mailto:secy-moef@nic.in";
+            var cc = "?cc=menong@cag.gov.in";
+            var subject = "&subject=Withdraw the draft EIA notification, " +
+                "2020 [F.N.2-50/2018/IA.III] and defer the process of public comments in the light of the " +
+                "Covid-2019 pandemic";
+            var body = "&body=" + StripHTML(template);
+            var url = to + cc + subject + body;
+            Process.Start(url);
+        }
+
+        protected void Form_Selection_Change(object sender, EventArgs e)
+        {
+            if(forms.SelectedValue == "EIANotification")
+            {
+                formPanel.Visible = true;
+            }
+            else
+            {
+                formPanel.Visible = false;
+            }
+        }
+
+        protected string readFile(string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourcePath = fileName;
+            resourcePath = assembly.GetManifestResourceNames()
+                    .Single(str => str.EndsWith(fileName));
+            
+            using (Stream stream = assembly.GetManifestResourceStream(resourcePath))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+        
+        protected void generateText()
+        {
+            string text = readFile("EIANotification.txt");
+            template = string.Format(text, firstName, lastName);
+            if (template != string.Empty)
+                SendEmail.Enabled = true;
+        }
+
+        protected void clearFields()
+        {
+            firstName = lastName = template = string.Empty;
+            SendEmail.Enabled = false;
+        }
+
+        public static string StripHTML(string input)
+        {
+            return HttpUtility.HtmlDecode(Regex.Replace(input, "<.*?>", String.Empty)).Replace("\r\n\r\n", " %0D ");
+        }
+
+        string firstName
+        {
+            get { return CandidateFirstName.Text; }
+            set { CandidateFirstName.Text = value; }
+        }
+        string lastName
+        {
+            get { return CandidateLastName.Text; }
+            set { CandidateLastName.Text = value; }
+        }
+        string template
+        {
+            get { return Template.Text; }
+            set { Template.Text = value; }
+        }
+    }
+}
